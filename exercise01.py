1f43bd306331fc69707588655a8103e59621b6ff
sebasvr8 <72099141+sebasvr8@users.noreply.github.com>
2020-09-29 14:42:54 -0600

    Add files via upload

---
+++ exercise01.py
@@ -0,0 +1,57 @@
+"""
+cliente:
+    quiero escribir un programa python que reciba de input del usuario
+    un nombre, un producto, costo
+    y se guarde en una lista con diccinarios, el registro es un diccionario
+    y va a ser guardado en una lista
+programador:
+    vaya
+
+cliente:
+    mira me gusta pero cada vez que inicio el programa, me borra el cliente anterior
+    y solo puedo guardar el que he ingresado en ese momento, no habria forma de
+    que se guarde a los 3 clientes que tengo?
+programador:
+    vaya
+mente:
+    y pollo no queres?
+
+cliente:
+    mira esta buenisimo peeeerroooooo, fijate que mi clientela a aumentado a veces tengo
+    3 a veces 5  a veces 10 osea ya no se cuantos clientes tendre en un dia, podrias
+    agregarle alguna forma de que yo le diga cuando quiero que se detenga y que me muestre
+    el reporte de lo que llevo en costo total hasta ese momento.
+programador:
+    vaya
+mente:
+    no te pide nada el gusto mono
+
+
+"""
+
+# creando la lista vacia
+listaRegistro = []
+clientes = 0
+NumClientes = int(input("Introduce cuantos clientes tendras el dia de hoy:  "))
+costoTotal = 0 
+
+while clientes < NumClientes:
+    cliente = input("nombre del cliente: ")
+    producto = input("producto: ")
+    costo = float(input("costo ($0.00): "))
+
+    # punto de programa
+    # registro = {"cliente": cliente, "producto": producto, "costo": costo}
+    registro = dict(cliente=cliente, producto=producto, costo=costo)
+    # como agrego un elemento nuevo a una lista?
+    listaRegistro.append(registro)
+    # dejar de ocupar la variable registro
+    # registro = None
+    clientes += 1
+    costoTotal = costoTotal + registro["costo"]
+
+for registro in listaRegistro:
+    print(registro)
+
+print ("El costo total es :")
+print(costoTotal)
